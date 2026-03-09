da398b7b2c960e667b84179bbdf282d6f5825d40
kbastin <kbastin@gmail.com>
2026-03-09 22:16:49 +0100

    Diamond squares.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use whiskers::prelude::*;
 
 #[sketch_app]
@@ -5,7 +7,7 @@ struct MySketch {
     /* add sketch parameters here */
     starting_square_side: f32,
     max_square_side: f32,
-    line_spacing: f32
+    line_spacing: f32,
 }
 
 impl Default for MySketch {
@@ -14,7 +16,7 @@ impl Default for MySketch {
             /* initialize sketch parameters to default values here */
             starting_square_side: 10.,
             max_square_side: 570.,
-            line_spacing: 13.
+            line_spacing: 13.,
         }
     }
 }
@@ -23,15 +25,17 @@ impl App for MySketch {
     fn update(&mut self, sketch: &mut Sketch, _ctx: &mut Context) -> anyhow::Result<()> {
         // draw code goes here
         sketch.color(Color::DARK_RED);
-
+        
         let mut side = self.starting_square_side;
-        sketch.rect(0., 0., side, side).rotate(90);
 
-        while  side <= self.max_square_side {
-            side+= self.line_spacing;
-            sketch.rect(0., 0., side, side).rotate(90);
+        sketch.rotate(PI / 4.);
+        sketch.rect(0., 0., side, side);
+
+        while side <= self.max_square_side {
+            side += self.line_spacing;
+            sketch.rect(0., 0., side, side);
         }
-
+        
         Ok(())
     }
 }
