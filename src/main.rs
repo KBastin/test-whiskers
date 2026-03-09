497c67c17bd0566d70d627d1fecd627ca4c200ba
kbastin <kbastin@gmail.com>
2026-03-09 21:50:52 +0100

    Update sketch

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use whiskers::prelude::*;
 #[sketch_app]
 struct MySketch {
     /* add sketch parameters here */
-    rectangle_width: f32,
-    rectangle_height: f32,
+    starting_square_side: f32,
+    max_square_side: f32,
+    line_spacing: f32
 }
 
 impl Default for MySketch {
     fn default() -> Self {
         Self {
             /* initialize sketch parameters to default values here */
-            rectangle_width: 150.,
-            rectangle_height: 50.,
+            starting_square_side: 10.,
+            max_square_side: 570.,
+            line_spacing: 13.
         }
     }
 }
@@ -20,9 +22,15 @@ impl Default for MySketch {
 impl App for MySketch {
     fn update(&mut self, sketch: &mut Sketch, _ctx: &mut Context) -> anyhow::Result<()> {
         // draw code goes here
-        sketch
-            .color(Color::DARK_RED)
-            .rect(200., 200., self.rectangle_width, self.rectangle_height);
+        sketch.color(Color::DARK_RED);
+
+        let mut side = self.starting_square_side;
+        sketch.rect(0., 0., side, side).rotate(90);
+
+        while  side <= self.max_square_side {
+            side+= self.line_spacing;
+            sketch.rect(0., 0., side, side).rotate(90);
+        }
 
         Ok(())
     }
